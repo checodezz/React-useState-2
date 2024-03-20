e23ca96e38a8d5f5725289af7f91c8cdee4d80ae
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-20 16:48:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./App.css";
+
+const Counter = () => {
+  const [initialValue, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{initialValue}</p>
+      <button onClick={() => setCount(initialValue + 1)}>Increment</button>
+      <button onClick={() => setCount(initialValue - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+const BackgroundToggler = () => {
+  const [isLightBlue, setColor] = useState(true);
+  return (
+    <div>
+      <button
+        style={{ background: isLightBlue ? "lightblue" : "grey" }}
+        onClick={() => setColor(!isLightBlue)}
+      >
+        Toggle Backgound
+      </button>
+    </div>
+  );
+};
+
+const MessageToggler = () => {
+  const [visibility, setVisibility] = useState(false);
+  const text = "This is a togglable message!";
+
+  return (
+    <div>
+      <button onClick={() => setVisibility(!visibility)}>
+        {visibility ? "Hide Message" : "Show Message"}
+      </button>
+      <p>{visibility ? text : ""}</p>
+    </div>
+  );
+};
+
+const TextColorToggler = () => {
+  const [greenColor, setColor] = useState(false);
+  return (
+    <div>
+      <p style={greenColor ? { color: "green" } : { color: "red" }}>
+        Text Color Toggler
+      </p>
+      <button onClick={() => setColor(!greenColor)}>Toggle Color</button>
+    </div>
+  );
+};
+/* 
+const FruitSelecctor = () => {
+  const fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+
+  const fruitsList = fruits.map((currentValue, index) => {
+    const [currentFruit, setFruitDetails] = useState(false);
+    const text = "You selected:" + currentFruit;
+    return (
+      <>
+        <li onClick={() => setFruitDetails(!currentFruit)}>{currentValue}</li>
+      </>
+    );
+  });
+
+  return (
+    <div>
+      <h2>Select a Fruit</h2>
+      <ul>{fruitsList}</ul>
+      <p>{currentFruit ? text : ""}</p>
+    </div>
+  );
+}; */
+
+
+const FruitSelector = () => {
+  const fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+
+}
+export default function App() {
+  return (
+    <main>
+      <Counter />
+      <hr />
+      <BackgroundToggler />
+      <hr />
+      <MessageToggler />
+      <hr />
+      <TextColorToggler />
+      <hr />
+      <FruitSelector />
+    </main>
+  );
+}
